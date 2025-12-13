3e496c9209f20a5a70776267033d20a1e9b9b4d6
Sahil Suman <sahilsuman1202@gmail.com>
2025-12-14 01:30:47 +0530

    Enhance styling and structure: update global styles, add button component, implement theme provider, and improve navbar with new links

--- components/web/navbar.tsx
+++ components/web/navbar.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
+import { buttonVariants } from "../ui/button";
 
 export function Navbar() {
 
     return (
-        <nav className="w-full py-5 flex items-center justify-between bg-brown-200 px-1">
+        <nav className="w-full py-3 flex items-center justify-between bg-[#dd8136] px-1">
         <div className="flex items-center gap-8">
             <Link href="/">
-            <img src="/chailogo6.png" alt="Logo" className="w-46 h-16"/> 
+            <h1 className="font-bold text-2xl tracking-wide text-[#f5e6d3]" style={{fontFamily: 'Playfair Display, Georgia, serif'}}>
+                Spill The <span className="text-amber-900">Chai</span> ☕</h1>
             </Link>
+
+            <div className="flex items-center gap-2">
+                <Link className={buttonVariants({variant: "ghost"})} href="/">Home</Link>
+                <Link className={buttonVariants({variant: "ghost"})} href="/blog">Blog</Link>
+                <Link className={buttonVariants({variant: "ghost"})} href="/create">Create</Link>
+            </div>
         </div>
+        
+        <div className="flex items-center gap-2">
+            <Link className={buttonVariants({variant: "outline"})} href="/auth/sign-up">Sign up</Link>
+            <Link className={buttonVariants({variant: "outline"})} href="/auth/login">Login</Link>
+        </div>
+
         </nav>
     )
 }
